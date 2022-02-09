7387f2664342bc816e50bb715ff0cf56c7ea8bb5
Connor McLaughlan <venom1539@gmail.com>
2022-02-09 02:16:09 -0700

    Init commit with android base files.

---
+++ Source/app/src/main/java/com/cmput301w22t36/codehunters/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cmput301w22t36.codehunters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
