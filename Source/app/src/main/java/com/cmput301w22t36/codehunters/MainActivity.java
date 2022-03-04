55cbdb9b2eb67cdfea9f6fd4711f5cdd6dd2c10d
Josh Wutzke <jwutzke@pop-os.localdomain>
2022-03-04 14:38:35 -0700

    Added basic MainActivity with placeholder fragments and navigation

--- Source/app/src/main/java/com/cmput301w22t36/codehunters/MainActivity.java
+++ Source/app/src/main/java/com/cmput301w22t36/codehunters/MainActivity.java
@@ -3,12 +3,62 @@ package com.cmput301w22t36.codehunters;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView codesNav, mapNav, socialNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.mainActivityFragmentView, MapFragment.class, null)
+                    .commit();
+        }
+
+        codesNav = findViewById(R.id.navToCodes);
+        mapNav = findViewById(R.id.navToMap);
+        socialNav = findViewById(R.id.navToSocial);
+
+
+        // Swap to the CodesFragment when the "Codes" textview is clicked
+        codesNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.mainActivityFragmentView, CodesFragment.class, null)
+                        .commit();
+            }
+        });
+
+        // Swap to the SocialFragment when the "Social" textview is clicked
+        socialNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.mainActivityFragmentView, SocialFragment.class, null)
+                        .commit();
+            }
+        });
+
+        // Swap to the MapFragment when the "Map" textview is clicked
+        mapNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.mainActivityFragmentView, MapFragment.class, null)
+                        .commit();
+            }
+        });
+
+
     }
 }
---
+++ Source/app/src/main/java/com/cmput301w22t36/codehunters/MapFragment.java
@@ -0,0 +1,43 @@
+package com.cmput301w22t36.codehunters;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MapFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MapFragment extends Fragment {
+
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static MapFragment newInstance() {
+        MapFragment fragment = new MapFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
