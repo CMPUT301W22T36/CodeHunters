d536cc8ed78019bac4050738165c4c223f37badf
Hua Wang <hua6@ualberta.ca>
2022-03-12 20:38:46 -0800

    All social layouts finished.

--- Source/app/src/main/java/com/cmput301w22t36/codehunters/SocialFragment.java
+++ Source/app/src/main/java/com/cmput301w22t36/codehunters/SocialFragment.java
@@ -1,12 +1,35 @@
 package com.cmput301w22t36.codehunters;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.zxing.integration.android.IntentIntegrator;
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +46,10 @@ public class SocialFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    TextView bestCodesNav, scoreBoardNav;
+    FloatingActionButton searchUser;
+    AlertDialog dialogSearchUser;
+    static Context context;
 
     public SocialFragment() {
         // Required empty public constructor
@@ -61,4 +88,78 @@ public class SocialFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_social, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (savedInstanceState == null) {
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.socialFragmentView, BestCodesFragment.class, null)
+                    .commit();
+        }
+        bestCodesNav = view.findViewById(R.id.navToBestCodes);
+        scoreBoardNav = view.findViewById(R.id.navToScoreBoard);
+        searchUser = view.findViewById(R.id.searchUser);
+        buildDialogSearchUser();
+
+        // Swap to the CodesFragment when the "Bestcodes" textview is clicked
+        bestCodesNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.socialFragmentView, BestCodesFragment.class, null)
+                        .commit();
+            }
+        });
+
+        // Swap to the SocialFragment when the "Scoreboard" textview is clicked
+        scoreBoardNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.socialFragmentView, ScoreBoardFragment.class, null)
+                        .commit();
+            }
+        });
+
+        searchUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogSearchUser.show();
+            }
+        });
+
+    }
+    public void buildDialogSearchUser() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        View view = getLayoutInflater().inflate(R.layout.dialog_searchuser, null);
+        EditText username = view.findViewById(R.id.user_name_searchText);
+        builder.setView(view)
+                .setTitle("Search User")
+                .setNegativeButton("Cancel",null)
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        if(!username.getText().toString().equals("")) {
+                            if(true){
+                                //todo
+                                getActivity().getSupportFragmentManager().beginTransaction()
+                                        .setReorderingAllowed(true)
+                                        .replace(R.id.mainActivityFragmentView, SearchUserFragment.class, null)
+                                        .addToBackStack("tag").commit();
+                                 }
+                            else{Toast.makeText(getActivity(), "User not exist", Toast.LENGTH_SHORT)
+                                    .show();}
+                        }
+                        else{Toast.makeText(getActivity(), "Please enter a user name.", Toast.LENGTH_SHORT)
+                                .show();}
+                    }
+                });
+        dialogSearchUser=builder.create();
+    }
+
 }
