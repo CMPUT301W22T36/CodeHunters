88f99f8a9a7488d36a12ec68011e7c2212d97403
ShalomiH <com.phyllo@gmail.com>
2022-03-14 13:18:02 -0600

    JUnit tests

---
+++ Source/app/src/test/java/com/cmput301w22t36/codehunters/UserUnitTest.java
@@ -0,0 +1,38 @@
+package com.cmput301w22t36.codehunters;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Class: UserUnitTest
+ * Test the User class.
+ */
+public class UserUnitTest {
+    // Mock object
+    private User mockUser() {
+        User user = new User();
+        return user;
+    }
+
+    @Test
+    void testAttributesNonQR() {
+        User user = mockUser();
+
+        String UUID1 = "142adbe77b149562";
+        ArrayList<String> accountUUIDs = new ArrayList<String>();
+        accountUUIDs.add(UUID1);
+        user.setAccountUUIDs(accountUUIDs);
+        String username = "John Doe";
+        user.setUsername(username);
+        String email = "example@gmail.com";
+        user.setEmail(email);
+
+        assertEquals(accountUUIDs, user.getAccountUUIDs());
+        assertEquals(username, user.getUsername());
+        assertEquals(email, user.getEmail());
+    }
+}
