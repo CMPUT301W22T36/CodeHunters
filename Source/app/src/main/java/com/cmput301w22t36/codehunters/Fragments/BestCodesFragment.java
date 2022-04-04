fe7ea6671c6edd0b9be0f699b70eaf9b8c2435a5
Connor McLaughlan <venom1539@gmail.com>
2022-04-04 17:32:07 -0600

    Refactor, removed all warnings

--- Source/app/src/main/java/com/cmput301w22t36/codehunters/Fragments/BestCodesFragment.java
+++ Source/app/src/main/java/com/cmput301w22t36/codehunters/Fragments/BestCodesFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -14,6 +13,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import com.cmput301w22t36.codehunters.Data.DataMappers.QRCodeMapper;
 import com.cmput301w22t36.codehunters.Data.DataTypes.QRCodeData;
@@ -38,20 +38,8 @@ import java.util.Observer;
 //              just for  now: No actual functions but all display and navigation are completed.
 public class BestCodesFragment extends Fragment {
     TextView title;
-    ListView bestcodes;
-    private ArrayAdapter<QRCode> codeArrayAdapter;
-    private ArrayList<QRCode> codeArrayList = new ArrayList<>();
-    private Observable tabChanger = new TabSetter();
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    ListView bestCodes;
+    private final Observable tabChanger = new TabSetter();
 
     public BestCodesFragment() {
         // Required empty public constructor
@@ -63,7 +51,6 @@ public class BestCodesFragment extends Fragment {
      *
      * @return A new instance of fragment SocialFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static BestCodesFragment newInstance(Observer thingWithTabs) {
         BestCodesFragment fragment = new BestCodesFragment();
         fragment.tabChanger.addObserver(thingWithTabs);
@@ -75,10 +62,6 @@ public class BestCodesFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -91,81 +74,77 @@ public class BestCodesFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         title = view.findViewById(R.id.bestCodesT);
-        bestcodes = view.findViewById(R.id.bestCodes);
+        bestCodes = view.findViewById(R.id.bestCodes);
 
-        // Get all qrcodes from database, then sort.
+        // Get all qrCodes from database, then sort.
         QRCodeMapper qm = new QRCodeMapper();
         qm.getAllCodes(qm.new CompletionHandler<ArrayList<QRCodeData>>() {
             @Override
             public void handleSuccess(ArrayList<QRCodeData> qrCodes) {
-                if (this != null) {
-                    rank(qrCodes);
-                }
+                rankAndSet(qrCodes);
             }
 
             @Override
             public void handleError(Exception e) {
             }
         });
-        bestcodes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            /**
-             * When a QRCode in the ListView is clicked, a dialog fragment will appear with the code's photo and location
-             * @param adapterView
-             * @param view
-             * @param i
-             * @param l
-             */
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                QRCode qrCodeClicked = (QRCode) bestcodes.getItemAtPosition(i);
-                Geolocation_PhotosFragment gpf = new Geolocation_PhotosFragment(qrCodeClicked);
 
-                // Retrieve image for clicked qr post:
-                QRCodeMapper qm = new QRCodeMapper();
-                if (qrCodeClicked.getPhotourl() != null) {
-                    qm.getImage(qrCodeClicked.getPhotourl(), qm.new CompletionHandler<Bitmap>() {
-                        @Override
-                        public void handleSuccess(Bitmap bMap) {
-                            qrCodeClicked.setPhoto(bMap);
-                            gpf.show(getActivity().getSupportFragmentManager(), "ADD_GEO");
-                        }
+        bestCodes.setOnItemClickListener((adapterView, view1, i, l) -> {
+            QRCode qrCodeClicked = (QRCode) bestCodes.getItemAtPosition(i);
+            Geolocation_PhotosFragment gpf = new Geolocation_PhotosFragment(qrCodeClicked);
 
-                        @Override
-                        public void handleError(Exception e) {
-                            gpf.show(getActivity().getSupportFragmentManager(), "ADD_GEO");
+            // Retrieve image for clicked qr post:
+            QRCodeMapper qm1 = new QRCodeMapper();
+            if (qrCodeClicked.getPhotourl() != null) {
+                qm1.getImage(qrCodeClicked.getPhotourl(), qm1.new CompletionHandler<Bitmap>() {
+                    @Override
+                    public void handleSuccess(Bitmap bMap) {
+                        qrCodeClicked.setPhoto(bMap);
+                        FragmentActivity fActivity = getActivity();
+                        if (fActivity != null) {
+                            gpf.show(fActivity.getSupportFragmentManager(), "ADD_GEO");
                         }
-                    });
-                }
-                else {
-                    gpf.show(getActivity().getSupportFragmentManager(), "ADD_GEO");
+                    }
+
+                    @Override
+                    public void handleError(Exception e) {
+                        FragmentActivity fActivity = getActivity();
+                        if (fActivity != null) {
+                            gpf.show(fActivity.getSupportFragmentManager(), "ADD_GEO");
+                        }
+                    }
+                });
+            }
+            else {
+                FragmentActivity fActivity = getActivity();
+                if (fActivity != null) {
+                    gpf.show(fActivity.getSupportFragmentManager(), "ADD_GEO");
                 }
             }
         });
-
     }
 
-    public void rank(ArrayList<QRCodeData> retrievedQrCodes){
+    public void rankAndSet(ArrayList<QRCodeData> retrievedQrCodes){
+        ArrayList<QRCode> codeArrayList = new ArrayList<>();
+
         for (int i = 0; i<retrievedQrCodes.size();i++){
             QRCode qrcode = new QRCode(retrievedQrCodes.get(i));
             codeArrayList.add(qrcode);
         }
 
         Collections.sort(codeArrayList, Collections.reverseOrder());
+
+        // Rank gets called in a different thread, need to ensure context is not null.
         Context context = this.getContext();
         if (context != null) {
-            codeArrayAdapter = new QRCodeAdapter(context, codeArrayList);
-            bestcodes.setAdapter(codeArrayAdapter);
+            ArrayAdapter<QRCode> codeArrayAdapter = new QRCodeAdapter(context, codeArrayList);
+            bestCodes.setAdapter(codeArrayAdapter);
         }
     }
-
 
     @Override
     public void onResume() {
         super.onResume();
         tabChanger.notifyObservers(0);
     }
-
-
-
-
 }
