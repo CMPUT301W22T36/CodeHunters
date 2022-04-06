a4d5a84b4390d030877a86cb7b819aa06f4441d8
Mehul Gadhia <mg@Mehuls-MacBook-Pro.local>
2022-04-05 20:33:51 -0600

    added capture javadocs

--- Source/app/src/main/java/com/cmput301w22t36/codehunters/Capture.java
+++ Source/app/src/main/java/com/cmput301w22t36/codehunters/Capture.java
@@ -2,6 +2,9 @@ package com.cmput301w22t36.codehunters;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
 
+/**
+ * Class declaration needed for image scanning in QRCode scan process
+ */
 public class Capture extends CaptureActivity {
 
 }
